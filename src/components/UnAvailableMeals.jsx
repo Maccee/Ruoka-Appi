6bf20a9b32d1e84eca57236bdabc7af87443d35e
Hasselgren Marcus <s2300288@BC-C02GR0BDQ6LR-MacBook-Air.local>
2023-09-06 13:07:43 +0300

    added missing items to shoppinglist

--- src/components/UnAvailableMeals.jsx
+++ src/components/UnAvailableMeals.jsx
@@ -10,6 +10,39 @@ const UnAvailableMeals = ({ recipe, fridgeItems }) => {
       !fridgeItem || Number(fridgeItem.quantity) < Number(ingredient.quantity)
     );
   });
+  const handleAddToShoppingList = () => {
+    // Retrieve the current shopping list from local storage
+    let currentShoppingList =
+      JSON.parse(localStorage.getItem("shoppingList")) || [];
+
+    // For each missing ingredient:
+    missingIngredients.forEach((missingItem) => {
+      // Check if the ingredient already exists in the shopping list
+      const existingItem = currentShoppingList.find(
+        (item) => item.name === missingItem.name
+      );
+
+      if (existingItem) {
+        // Ensure the quantities are numbers, then add them together
+        existingItem.quantity =
+          Number(existingItem.quantity) + Number(missingItem.quantity);
+      } else {
+        // If it doesn't, add the missing ingredient to the shopping list
+        currentShoppingList.push(missingItem);
+      }
+    });
+
+    // Store the updated shopping list in local storage
+    localStorage.setItem("shoppingList", JSON.stringify(currentShoppingList));
+
+    // Log the added or updated items
+    console.log(
+      "Items added or updated in the shopping list:",
+      missingIngredients
+    );
+
+    alert("Items added to the shopping list!");
+  };
 
   return (
     <li className="listMeals greyedOut">
@@ -37,6 +70,7 @@ const UnAvailableMeals = ({ recipe, fridgeItems }) => {
               </li>
             ))}
           </ul>
+          <button onClick={handleAddToShoppingList}>Ostoslistalle</button>
         </>
       )}
     </li>
